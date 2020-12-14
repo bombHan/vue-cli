9487fbb06787b82d17163adc02cf3b905a9b9634
hansh <747263288@qq.com>
2020-12-14 19:06:41 +0800

    commit

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,48 @@
 
+import that from '../main'
+//下载链接地址图片
+function downloadByBlob(url, name) {
+  let image = new Image()
+  image.setAttribute('crossOrigin', 'anonymous')
+  image.src = url
+  image.onload = () => {
+    let canvas = document.createElement('canvas')
+    canvas.width = image.width
+    canvas.height = image.height
+    let ctx = canvas.getContext('2d')
+    ctx.drawImage(image, 0, 0, image.width, image.height)
+    canvas.toBlob((blob) => {
+      let url = URL.createObjectURL(blob)
+      let eleLink = document.createElement('a')
+      eleLink.download = name
+      eleLink.href = url
+      eleLink.click()
+      eleLink.remove()
+      // 用完释放URL对象
+      URL.revokeObjectURL(url)
+    })
+  }
+}
+
+//复制字符串
+function copyString(str) {
+  const input = document.createElement("input");
+  input.value = str;
+  input.readOnly = true;
+  document.body.appendChild(input);
+  input.select();
+  input.setSelectionRange(0, input.value.length);
+  document.execCommand("Copy");
+  document.body.removeChild(input);
+  that.$message.success("复制成功");
+}
+
 // 将对象转换成url后可带的参数字符串
 function objecToQueryString(obj) {
   const keyArr = Object.keys(obj);
   let str = '?';
   keyArr.forEach((item) => {
-    if(obj[item] != null) {
+    if (obj[item] != null) {
       str += `${item}=${obj[item]}&`
     }
   })
@@ -12,7 +50,7 @@ function objecToQueryString(obj) {
 }
 
 /* 深拷贝 */
-function deepClone (source) {
+function deepClone(source) {
   if (!source && typeof source !== 'object') {
     throw new Error('error arguments', 'shallowClone')
   }
@@ -32,7 +70,7 @@ function deepClone (source) {
 
 
 
-function getQueryObject (url) {
+function getQueryObject(url) {
   url = url == null ? window.location.href : url
   const search = url.substring(url.lastIndexOf('?') + 1)
   const obj = {}
@@ -48,7 +86,7 @@ function getQueryObject (url) {
 }
 
 // 防抖动debounce
-function _debounce (fn, delay) {
+function _debounce(fn, delay) {
   delay = delay || 600
   let timer
   return function () {
@@ -66,40 +104,51 @@ function _debounce (fn, delay) {
 
 import axios from 'axios';
 function exportExcel(url, options = {}) {
-    return new Promise((resolve, reject) => {
-        console.log(`${url} 请求数据，参数=>`, JSON.stringify(options))
-        axios.defaults.headers['content-type'] = 'application/json;charset=UTF-8'
-        axios({
-            method: 'post',
-            url: url, // 请求地址
-            data: options, // 参数
-            responseType: 'blob' // 表明返回服务器返回的数据类型
-        }).then(
-            response => {
-                resolve(response.data)
-                let blob = new Blob([response.data], {
-                    type: 'application/vnd.ms-excel'
-                })
-                console.log(blob, response)
-                let fileName = response.headers['content-disposition'] ? response.headers['content-disposition'] : Date.parse(new Date()) + '.xlsx'
-                if (window.navigator.msSaveOrOpenBlob) {
-                    // console.log(2)
-                    navigator.msSaveBlob(blob, fileName)
-                } else {
-                    // console.log(3)
-                    var link = document.createElement('a')
-                    link.href = window.URL.createObjectURL(blob)
-                    link.download = fileName
-                    link.click()
-                    //释放内存
-                    window.URL.revokeObjectURL(link.href)
-                }
-            },
-            err => {
-                reject(err)
-            }
-        )
-    })
+  return new Promise((resolve, reject) => {
+    console.log(`${url} 请求数据，参数=>`, JSON.stringify(options))
+    axios.defaults.headers['content-type'] = 'application/json;charset=UTF-8'
+    axios({
+      method: 'post',
+      url: url, // 请求地址
+      data: options, // 参数
+      responseType: 'blob' // 表明返回服务器返回的数据类型
+    }).then(
+      response => {
+        resolve(response.data)
+        let blob = new Blob([response.data], {
+          type: 'application/vnd.ms-excel'
+        })
+        console.log(blob, response)
+        let fileName = response.headers['content-disposition'] ? response.headers['content-disposition'] : Date.parse(new Date()) + '.xlsx'
+        if (window.navigator.msSaveOrOpenBlob) {
+          // console.log(2)
+          navigator.msSaveBlob(blob, fileName)
+        } else {
+          // console.log(3)
+          var link = document.createElement('a')
+          link.href = window.URL.createObjectURL(blob)
+          link.download = fileName
+          link.click()
+          //释放内存
+          window.URL.revokeObjectURL(link.href)
+        }
+      },
+      err => {
+        reject(err)
+      }
+    )
+  })
+}
+
+// 将图片转成base64
+function drawBase64Image (img) {
+  var canvas = document.createElement('canvas')
+  canvas.width = img.width
+  canvas.height = img.height
+  var ctx = canvas.getContext('2d')
+  ctx.drawImage(img, 0, 0, img.width, img.height)
+  var dataURL = canvas.toDataURL('image/png')
+  return dataURL
 }
 
 
@@ -108,7 +157,10 @@ let utils = {
   deepClone,
   getQueryObject,
   _debounce,
-  exportExcel
+  exportExcel,
+  downloadByBlob,
+  copyString,
+  drawBase64Image
 }
 
 export default utils;
